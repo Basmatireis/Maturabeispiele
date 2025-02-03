9486d72366faf9497f7d3dd9296009577cac609d
Noel <hauke.noel@icloud.com>
2025-02-03 01:41:22 +0100

    thema 16

--- Python/Thema 16/Aufgabe_1.py
+++ Python/Thema 16/Aufgabe_1.py
@@ -1,9 +1,16 @@
+# Definiere das Einkommen
 for income in [45000]: 
+    # Überprüfe, ob das Einkommen kleiner oder gleich 10.000 ist
     if income <= 10_000: 
+        # Setze die Steuer auf 0
         tax = 0 
+    # Überprüfe, ob das Einkommen kleiner oder gleich 20.000 ist
     elif income <= 20_000:  
+        # Berechne die Steuer für Einkommen zwischen 10.000 und 20.000
         tax = (income - 10_000) * 0.1               
     else: 
+        # Berechne die Steuer für Einkommen über 20.000
         tax = (income-20_000) * 0.2 +  10_000*0.1  
 
-    print(f"For example, suppose that the taxable income is ${income} the income tax payable is $10000*0% + $10000*10%  + $25000*20% = ${tax}. ") 
+    # Drucke das Ergebnis
+    print(f"For example, suppose that the taxable income is ${income} the income tax payable is $10000*0% + $10000*10%  + $25000*20% = ${tax}. ")
--- Python/Thema 16/Aufgabe_2.py
+++ Python/Thema 16/Aufgabe_2.py
@@ -1,4 +1,8 @@
+# Schleife durch die Zahlen 1 bis 10
 for x in range(1, 11): 
+    # Drucke eine neue Zeile
     print("") 
+    # Schleife durch die Zahlen 1 bis 10
     for y in range(1, 11): 
+        # Drucke das Produkt von x und y, ohne Zeilenumbruch
         print(x * y, end=" ")
